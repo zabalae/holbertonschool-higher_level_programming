8e1fe85fe1c1ed93238590dee982678ae1de1ac5
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-26 21:10:55 -0400

    Task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,8 @@ def print_last_digit(number):
     if number < 0:
         last_digit = number % -10
         return last_digit
+    elif number == 0:
+        return 0
     else:
         last_digit = number % 10
         return last_digit
