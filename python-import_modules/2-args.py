fe21ee9477960ab82a33e650e3275ce2c35578f0
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-28 09:56:44 -0400

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    if args == 0 or args > 1:
-        if args == 0:
-            print("{} arguments.".format(args))
-        else:
-            print("{} arguments:".format(args))
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
     else:
-        print("{} argumnent:".format(args))
+        print("{} argumnents:".format(args))
     index = 1
     while index < (len(sys.argv)):
         print("{:d}: {:s}".format(index, sys.argv[index]))
