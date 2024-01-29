52a5a5168eb21bba43883cfe6ca94440508f31cc
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-28 20:36:09 -0400

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_str = [x for x in new_string if x != 'c' and x != 'C']
-    return ("".join(new_str))
+    str_list = list(my_string)
+    string = ''
+    for ch in str_list:
+        if ch != 'C' and ch != 'c':
+            string += ch
+        return string
 
 
