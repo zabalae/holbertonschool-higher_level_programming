4cb52cf3201692b468885ddedd4c2e4fdf57e82d
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-28 21:47:05 -0400

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_int = my_list[0]
-    for x in my_list:
-        if x > max_int:
-            max_int = x
-    return x
+    else:
+        max_int = my_list[0]
+        for x in range(len(my_list)):
+            if my_list[x] > max_int:
+                max_int = my_list[x]
+        return max_int
