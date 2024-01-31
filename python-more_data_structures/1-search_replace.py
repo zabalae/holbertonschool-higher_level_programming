ef993112b644c256d3b16c2c029a9461af05296b
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-31 16:46:20 -0400

    Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,5 +2,7 @@
 def search_replace(my_list, search, replace):
     for x in range(len(my_list)):
         if my_list[x] == search:
-            my_list[x] == replace
+            new_list[x] == replace
+        else:
+            new_list[x] == my_list[x]
     return my_list
