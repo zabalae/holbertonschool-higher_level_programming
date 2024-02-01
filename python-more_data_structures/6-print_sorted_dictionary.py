82735dc15a6fc02698ed94be55cd9809bc3f87b8
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-31 20:02:40 -0400

    Task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted _dictionary(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print(f"{key}: {a_dictionary[key]}")
