dcacdf4678517c0506055ac07c4a19e617d2029c
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-08 13:23:09 -0400

    Task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     '''Class that represents a rectangle'''
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
