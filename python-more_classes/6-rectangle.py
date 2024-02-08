66329a06c6c3ada0f466eacd1a9bb88edbf5e1d8
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-08 11:30:24 -0400

    Task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
