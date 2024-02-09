8b42862c80776b06ef29e2a44b0385d656d8049d
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-09 12:43:34 -0400

    Task 5

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Class MyList that inherits from list:
+'''
+
+
+def print_sorted(self):
+    '''Public instance method that prints the list sorted'''
+    def print_sorted(self):
+        print(sorted(self))
