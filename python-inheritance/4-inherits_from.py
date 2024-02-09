b7c6a11383cae169423a6102d9a1fe36faba6eeb
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-09 19:16:29 -0400

    Task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ instance of a class that inherited from the specified class
 
 def inherits_from(obj, a_class):
     '''inherits_from function'''
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
