417ce1ce150ba2307a1ddcbd0ea431919947af48
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-10 15:32:38 -0400

    Task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''class BaseGeometry'''
+
+
+def area(self):
+    Exception("area() is not implemented")
+
+def integer_validator(self, name, value):
+    if type(value) is not int:
+        raise TypeError("{:s} must be an integer".format(name))
+    if value <= 0:
+        raise ValueError("{:s} must be greater than 0".format(name))
