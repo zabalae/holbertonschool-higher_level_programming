1188b3a8637c43a66bce905e32c371c8d647b5e7
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-11 18:10:48 -0400

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,3 +10,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
