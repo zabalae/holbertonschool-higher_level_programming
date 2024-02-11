e6adb234aa87cadc8c16436c1552e5b86c1770b4
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-11 19:40:29 -0400

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''function that creates an Object from a JSON file'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''load_from_json_file function'''
+    with open(filename) as f:
+        return json.load(f)
