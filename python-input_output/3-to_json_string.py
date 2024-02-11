d61c205510333baea86a46ab2acecd290e870fa3
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-11 19:22:25 -0400

    Task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''function that returns the JSON representation of an object'''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''to_json_string function'''
+    return json.dumps(my_obj)
