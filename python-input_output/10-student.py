454292e8f78b227efc2dd78e40f60a27305d5d02
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-11 20:21:52 -0400

    Task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,7 +11,6 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if (type(attrs) is list and
-            all(type(ele) == str for ele in attrs)):
+        if (type(attrs) is list and all(type(ele) is str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
