f9f020bc36e0cc7dd1749dc853331e49ebdd8f59
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-14 01:57:43 -0400

    Task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -53,7 +53,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return self.__y
+        return (self.__y)
 
     @y.setter
     def y(self, value):
@@ -64,5 +64,5 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        return (self.__width * self.__height)
+        return (self.width * self.height)
 
