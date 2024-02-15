745cecc2fe674bb8b9f202ddf19b9330bb2ff9d3
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-15 14:37:58 -0400

    Task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,16 @@ class Rectangle(Base):
         s = "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
         return s
+
+    def update(self, *args):
+        '''assign an argument to each attribute'''
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
