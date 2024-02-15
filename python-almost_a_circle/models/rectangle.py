27bbe8dbe8d723c1c4af3dd33f706451938396d1
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-14 22:52:36 -0400

    Task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -86,5 +86,6 @@ class Rectangle(Base):
 
     def __str__(self):
         '''Return a string representation of rectangle'''
-        s = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        s = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
         return s
