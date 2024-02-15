0a4c88d568bfd548c03a0abbfe15ec05dc42a7b2
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-14 20:19:34 -0400

    Task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,10 +17,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''get width'''
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        '''set width'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -29,10 +31,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        '''get height'''
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        '''set height'''
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -41,10 +45,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        '''get x'''
         return (self.__x)
 
     @x.setter
     def y(self, value):
+        '''set x'''
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -53,10 +59,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        '''get y'''
         return (self.__y)
 
     @y.setter
     def y(self, value):
+        '''set y'''
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -64,9 +72,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        '''calculate the area'''
         return (self.width * self.height)
     
     def display(self):
+        '''method that prints in stdout'''
         if self.width == 0 or self.height == 0:
             print("")
             return
