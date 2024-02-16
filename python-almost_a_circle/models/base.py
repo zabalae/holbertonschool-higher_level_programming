e74599f7c203a145091e4ced0ea52e164ec6f56f
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-16 00:29:52 -0400

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,14 +2,27 @@
 '''Class Base'''
 
 
+import json
+
+
 class Base:
     '''Class Base'''
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''Initialization of Base'''
 
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ 
+        returns JSON representation of list_dictionaries 
+        """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
