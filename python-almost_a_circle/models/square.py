ce270fa82fcb660fd8d965a510fe4aaa48f6f9bf
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-15 21:33:05 -0400

    Task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Square class'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Initialize a square'''
+        super().__init__(size, size, x, y, id)
+        self.size = size
