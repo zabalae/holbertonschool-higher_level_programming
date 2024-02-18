d3c87f05c4fdc4e47a6db690ed9c484456069e56
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 12:49:04 -0400

    base tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -6,6 +6,7 @@ import unittest
 import io
 from models.base import Base
 from models.square import Square
+import sys
 
 
 class TestSquare(unittest.TestCase):
@@ -22,3 +23,40 @@ class TestSquare(unittest.TestCase):
         '''Test area'''
         square1 = Square(40)
         self.assertEqual(square1.area(), 40 * 40)
+
+    def oneArgument(self):
+        square1 = Square(15)
+        square2 = Square(16)
+        self.assertEqual(square1.id, square2.id -1)
+
+    def twoArguments(self):
+        square1 = Square(10, 5)
+        square2 = Square(5, 10)
+        self.assertEqual(square1.id, square2.id -1)
+
+    @staticmethod
+    def capture_stdout(sq, method):
+        capture = io.StringIO()
+        sys.stdout = capture
+        if method == "print":
+            print(sq)
+        else:
+            sq.display()
+        sys.stdout = sys.__stdout__
+        return capture
+
+    def strPrintSize(self):
+        s = Square(4)
+        capture = TestSquare.capture_stdout(s, "print")
+        correct = "[Square] ({}) 0/0 - 4\n".format(s.id)
+        self.assertEqual(correct, capture.getvalue())
+
+    def displaySize(self):
+        s = Square(2, 0, 0, 9)
+        capture = TestSquare.capture_stdout(s, "display")
+        self.assertEqual(" ###\n##\n", capture.getvalue())
+
+    def updateArguments(self):
+        s = Square(10, 10, 10, 10)
+        s.update()
+        self.assertEqual("[Square] (10) 10/10 - 10", str(s))
