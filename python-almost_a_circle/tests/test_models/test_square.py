3682bb6d196e059e8ac7bee510e74698dc421324
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 13:13:44 -0400

    base tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -24,13 +24,3 @@ class TestSquare(unittest.TestCase):
         square1 = Square(40)
         self.assertEqual(square1.area(), 40 * 40)
 
-    def oneArgument(self):
-        square1 = Square(15)
-        square2 = Square(16)
-        self.assertEqual(square1.id, square2.id -1)
-
-    def twoArguments(self):
-        square1 = Square(10, 5)
-        square2 = Square(5, 10)
-        self.assertEqual(square1.id, square2.id -1)
-
