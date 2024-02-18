71cee92ad66e5f4d34c38641ce86db83d09ee6af
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 09:44:40 -0400

    base tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -103,3 +103,26 @@ class TestBase(unittest.TestCase):
         recs = Rectangle.load_from_file()
         self.assertEqual(type(recs), list)
         self.assertEqual(len(recs), 0)
+
+class TestRectangle(unittest.TestCase):
+    '''Tests for rectangle.py'''
+    def test_all_attr_given(self):
+        """Test all attributes match what's given"""
+        r1 = Rectangle(15, 40, 5, 2, 89)
+        self.assertTrue(r1.width == 15)
+        self.assertTrue(r1.height == 40)
+        self.assertTrue(r1.x == 5)
+        self.assertTrue(r1.y == 2)
+        self.assertTrue(r1.id == 89)
+
+    def test_zero_height(self):
+        """Tests whn the height is zero"""
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+
+    def test_rec_area(self):
+        """Tests the area of the rectangle"""
+        self.assertEqual(Rectangle(4, 5).area(), 20)
+        self.assertEqual(Rectangle(2, 3, 0, 0, 7).area(), 6)
+        self.assertEqual(Rectangle(8, 20, 3, 4, 76).area(), 160)
+        self.assertEqual(Rectangle(4, 7, 2, 1).area(), 28)
