18613a8413bf6c58d3c90b0227921e24d428d62c
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 21:40:05 -0400

    base tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -20,9 +20,5 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle()
 
-    def test_None_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(None, 2)
-
 if __name__ == "__main__":
     unittest.main()
