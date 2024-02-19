c9341fdfb3499824dd33068f7f7c5788c41f07af
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 21:15:00 -0400

    base tests

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Test Cases for rectangle class'''
+
+from models.base import Base
+from models.rectangle import Rectangle
+import os
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    def test_zero_height(self):
+        """Tests whn the height is zero"""
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
