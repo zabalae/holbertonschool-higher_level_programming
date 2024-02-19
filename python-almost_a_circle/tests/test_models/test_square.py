0e00675a836b3d3602856d83f6486e5d5e34cdb9
Elizabeth <elizabeth.zabala3@gmail.com>
2024-02-18 21:19:34 -0400

    base tests

---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Test cases for square class'''
+
+
+import unittest
+import io
+from models.base import Base
+from models.square import Square
+import sys
+
+
+class TestSquare(unittest.TestCase):
+    '''Test Cases for square class'''
+    pass
