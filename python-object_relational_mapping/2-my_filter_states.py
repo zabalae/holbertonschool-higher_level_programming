c9a83e478e20042105d4e9a98e5644f6c794ab9d
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 15:17:12 -0400

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values"""
-"""in the states table where name matches the argument"""
+"""Script that takes in an argument and displays all values
+in the states table where name matches the argument"""
 
 
 import MySQLdb
