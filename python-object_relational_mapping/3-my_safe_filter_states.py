bcbdf682c18aa471f217899ea72b2d0e5840939a
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 16:10:30 -0400

    Task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,7 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE \
-                   %s ORDER BY \
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
                    states.id ASC",(sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
