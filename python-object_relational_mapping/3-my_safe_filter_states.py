3dc41fee9115b85b6e98d2d06f83690407349576
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 15:27:29 -0400

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script that takes in an argument and displays all values
+in the states table where name matches the argument"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE \
+                   %s ORDER BY \
+                   states.id ASC",(sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
