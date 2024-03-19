ad0428eb5f953fcb4c307da1b30442b02099873a
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 12:55:48 -0400

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
     rows = cursor.fetchall()
