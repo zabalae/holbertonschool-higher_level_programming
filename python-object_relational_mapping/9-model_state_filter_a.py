b2b9770ee4256ddcd7a89d86ee88e8afa8b44cf7
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 23:17:14 -0400

    Task 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
     states = session.query(State).filter(
         State.name.contains('a')).order_by(State.id.asc())
-    
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
