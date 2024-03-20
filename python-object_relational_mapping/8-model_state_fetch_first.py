7160c00bea2ce809cf8c4a952586bdba542f5b17
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-19 23:06:34 -0400

    Task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     state = session.query(State).first()
 
     if state is not None:
